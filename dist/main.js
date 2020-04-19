d9dc0a099ec34dcd933e053f84c8edf3ea82957e
cauil <xiaoloujun@gmail.com>
2020-04-19 16:33:07 +0800

    first commit

---
+++ dist/main.js
@@ -0,0 +1,24 @@
+(function(modules) {
+      function require(filename) {
+        var fn = modules[filename]
+        var module = {exports: {}}
+
+        fn(require, module, module.exports)
+
+        return module.exports
+      }
+
+      require('/Users/lcf/workspace/lab/webpack-simple/src/index.js')
+    })({ '/Users/lcf/workspace/lab/webpack-simple/src/index.js': function(require, module, exports) {"use strict";
+
+var _greeting = require("./greeting.js");
+
+document.write((0, _greeting.greeting)('lcf'));},'./greeting.js': function(require, module, exports) {"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.greeting = greeting;
+function greeting(name) {
+  return 'hello' + name;
+}}, })
